dcaa034cc0583ad36922a238a45e4a6be2d37fd6
karan <karanbhati1310@gmail.com>
2025-07-17 16:46:13 +0530

    hero text

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -15,14 +15,14 @@ export default function HeroSlipstream() {
         />
         {/* Overlay Content */}
         <div className="absolute inset-0 bg-black/30" />
-        <div className="absolute top-[52%] md:top-[60%] left-4 md:left-20 -translate-y-1/2 text-white z-10 max-w-xl">
+        <div className="absolute top-[60%] md:top-[60%] left-4 md:left-20 -translate-y-1/2 text-white z-10 max-w-xl">
           <h1 className="text-5xl md:text-6xl font-bold leading-tight">
             DRIVE WITH DISTINCTION
           </h1>
-          <p className="mt-4 text-lg md:text-xl text-gray-100 max-w-md">
+          <p className="mt-2 md:mt-4 text-lg md:text-xl text-gray-100 max-w-xs md:max-w-md">
              Premium wheels and accessories built for performance, precision, and style.
           </p>
-          <button className="mt-6 px-4 md:px-6 py-2 md:py-3 bg-white text-black font-semibold text-base rounded hover:bg-gray-200 transition">
+          <button className="mt-4 md:mt-6 px-4 md:px-6 py-2 md:py-3 bg-white text-black font-semibold text-sm md:text-base rounded hover:bg-gray-200 transition">
             Explore Collections
           </button>
         </div>
